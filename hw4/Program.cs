76be2e45897e4bd1d80b595c3303398ffd194507
luckydn <sp.donbass@gmail.com>
2022-06-29 12:08:51 +0300

    выполнил домашнее задание. задача 4

---
+++ hw4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите три числа, что бы программа определила большее из них");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+
+if(numberA > max) max = numberA;
+if(numberB > max) max = numberB;
+if(numberC > max) max = numberC;
+
+Console.WriteLine($"Число {max} является найбольшим");
