4233b54a0e9ddb7d417d5bcc8470ea444143597e
luckydn <sp.donbass@gmail.com>
2022-06-29 13:31:00 +0300

    выполнил ДЗ задача 8

---
+++ hw8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число, что бы программа определила все четные числа до него");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i <= number; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write("{0} ", i);
+    }
+}
+
+
