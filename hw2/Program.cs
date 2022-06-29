78395aa8fc4642b50b115a99bada2fd9a698a41e
luckydn <sp.donbass@gmail.com>
2022-06-29 12:05:27 +0300

    выполнил домашнее задание, задача 2

---
+++ hw2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите два числа, что бы программа определила большее из них");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+
+if(numberA > numberB) max = numberA;
+if(numberB > numberA) max = numberB;
+
+Console.WriteLine($"Число {max} является найбольшим");
