762bf26e0b3a93acaaaa60394789f7f844e1db4c
luckydn <sp.donbass@gmail.com>
2022-06-29 12:28:21 +0300

    Выполнил ДЗ. Задача 6

---
+++ hw6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число, что бы программа определила является ли оно четным");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("число четное");
+}
+else
+{
+    Console.WriteLine("число не четное");
+}
